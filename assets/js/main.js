929029cce840cae6f03695f2fac1a3358404800a
MariaDemianchuk <106021153+MariaDemianchuk@users.noreply.github.com>
2022-08-20 11:42:23 +0300

    Added link of map

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementsByClassName('footer__map'), {
+        center: {
+            lat: 50.12071074819811,
+            lng: 26.818001965327685,
+        },
+        zoom: 15,
+
+    })
+}
